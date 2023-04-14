0d37f59a5ecf8cc53b19ca146c5a822051bd7945
Tiyada Klaiklang <yaya2943@gmail.com>
2023-04-14 15:21:21 +0000

    Update 4 files

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -1,12 +1,12 @@
 export default [
-    {
-        name: "room",
-        type: "glbModel",
-        path: "/models/Finale Version 16.glb",
-    },
-    {
-        name: "screen",
-        type: "videoTexture",
-        path: "/textures/kda.mp4",
-    },
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/final room.glb",
+  },
+  {
+    name: "screen",
+    type: "videoTexture",
+    path: "/textures/kda.mp4",
+  },
 ];
