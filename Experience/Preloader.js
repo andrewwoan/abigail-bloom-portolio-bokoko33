999e354b581b51da73744124ef269afad3cee1a1
Shreyansh Rajput <96108214+kiritoInd@users.noreply.github.com>
2024-03-01 14:54:42 +0530

    Update Preloader.js

--- Experience/Preloader.js
+++ Experience/Preloader.js
@@ -33,7 +33,6 @@ export default class Preloader extends EventEmitter {
 
         this.room = this.experience.world.room.actualRoom;
         this.roomChildren = this.experience.world.room.roomChildren;
-        console.log(this.roomChildren);
     }
 
     firstIntro() {
