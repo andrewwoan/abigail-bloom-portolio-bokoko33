28a1244d0c45fa11a9681a77238b2aaa96bebdd3
Shreyansh Rajput <96108214+kiritoInd@users.noreply.github.com>
2024-03-02 15:56:12 +0530

    Update Room.js

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -31,7 +31,6 @@ export default class Room {
 
             if (child instanceof THREE.Group) {
                 child.children.forEach((groupchild) => {
-                    console.log(groupchild.material);
                     groupchild.castShadow = true;
                     groupchild.receiveShadow = true;
                 });
